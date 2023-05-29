1bde5267260f8162fa53923bd1c049fa68f8f1fc
Juadeb <juadebgabriel@gmail.com>
2023-05-29 09:55:47 +0100

    fix

---
+++ index.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
