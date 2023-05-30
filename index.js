6894561721799729185de0cf70e6ffd7a2756ee5
Juadeb <juadebgabriel@gmail.com>
2023-05-30 14:52:50 +0100

    fix

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const navList = document.getElementById("navList");
+const toggleBtn = document.getElementById("toggleBtn");
+
+// navList.style.display = "block"; // Hide the navList initially
+
+toggleBtn.addEventListener("click", () => {
+  if (navList.style.display === "none") {
+    navList.style.display = "block"; // Show the navList when button is clicked
+  } else {
+    navList.style.display = "none"; // Hide the navList when button is clicked again
+  }
+});
+
+// Add this code to handle page refresh
+window.addEventListener("load", () => {
+  navList.style.display = "none"; // Ensure navList is hidden on page load
+});
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
